04761b9e82fef2513f4796667ec1a24fd32a6cca
berathamzaoglu <berat.hamzaoglu@hotmail.com>
2020-03-01 01:20:08 +0300

    Update default_weekday_labels_row.dart

--- lib/default_weekday_labels_row.dart
+++ lib/default_weekday_labels_row.dart
@@ -5,13 +5,75 @@ class CalendarroWeekdayLabelsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Expanded(child: Text("Mon", textAlign: TextAlign.center)),
-        Expanded(child: Text("Tue", textAlign: TextAlign.center)),
-        Expanded(child: Text("Wed", textAlign: TextAlign.center)),
-        Expanded(child: Text("Thu", textAlign: TextAlign.center)),
-        Expanded(child: Text("Fri", textAlign: TextAlign.center)),
-        Expanded(child: Text("Sat", textAlign: TextAlign.center)),
-        Expanded(child: Text("Sun", textAlign: TextAlign.center)),
+        Expanded(child: Text("Mon", textAlign: TextAlign.center,
+                       
+    style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),
+
+                            
+                            
+                            ),),
+        Expanded(child: Text("Tue", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
+        Expanded(child: Text("Wed", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
+        Expanded(child: Text("Thu", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
+        Expanded(child: Text("Fri", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
+        Expanded(child: Text("Sat", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
+        Expanded(child: Text("Sun", textAlign: TextAlign.center,style: TextStyle(
+    fontFamily: 'Poppins',
+    color: Color(0xff4a4a4a),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 0,
+    
+    ),)),
       ],
     );
   }
